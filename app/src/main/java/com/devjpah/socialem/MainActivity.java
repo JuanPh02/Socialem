7e15a531e7a4d7b5467964f51127c695735a1a33
Juan Pablo Arroyave <50724493+JuanPh2002@users.noreply.github.com>
2021-10-23 12:41:41 -0500

    Navigation in Bottom Bar (Blog and Profile Fragments)

--- app/src/main/java/com/devjpah/socialem/MainActivity.java
+++ app/src/main/java/com/devjpah/socialem/MainActivity.java
@@ -1,10 +1,14 @@
 package com.devjpah.socialem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,13 +17,18 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabItem;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import nl.joery.animatedbottombar.AnimatedBottomBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    AnimatedBottomBar bottomAppBar;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    AnimatedBottomBar animatedBottomBar;
     BottomNavigationView bottomNavigationView;
     FloatingActionButton fabAdd;
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +36,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         conectar();
 
+        if (savedInstanceState == null) {
+            animatedBottomBar.selectTabById(R.id.it_blog, true);
+            fragmentManager = getSupportFragmentManager();
+            BlogFragment blogFragment = new BlogFragment();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, blogFragment).commit();
+        }
+        animatedBottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int i, @Nullable AnimatedBottomBar.Tab tab, int i1, @NotNull AnimatedBottomBar.Tab tab1) {
+                Fragment fragment = null;
+                switch (tab1.getId()) {
+                    case R.id.it_blog:
+                        fragment = new BlogFragment();
+                        break;
+                    case R.id.it_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+
+                if(fragment != null) {
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                } else {
+                    Log.e(TAG, "Error in creating fragment");
+                }
+            }
+
+            @Override
+            public void onTabReselected(int i, @NotNull AnimatedBottomBar.Tab tab) {
+
+            }
+        });
+
+        //Events
         fabAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
+
         //Badges
-        //bottomAppBar.setBadgeAtTabIndex(0, new AnimatedBottomBar.Badge('99',));
+        animatedBottomBar.setBadgeAtTabIndex(0, new AnimatedBottomBar.Badge());
     }
 
     private void conectar() {
-        bottomAppBar = findViewById(R.id.bottom_app_bar);
+        animatedBottomBar = findViewById(R.id.animated_bottom_bar);
         fabAdd = findViewById(R.id.fab_add);
     }
 }
