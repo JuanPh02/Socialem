53858c5e2554b9c518662934dfb343c2b8582fee
Tiwar02 <50062640+Tiwar02@users.noreply.github.com>
2021-10-27 23:05:09 -0500

    Update register

--- app/src/main/java/com/devjpah/socialem/EditProfileActivity.java
+++ app/src/main/java/com/devjpah/socialem/EditProfileActivity.java
@@ -10,13 +10,22 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Date;
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class EditProfileActivity extends AppCompatActivity {
 
+    FirebaseFirestore bd = FirebaseFirestore.getInstance();
+    HashMap<String,Object> map = new HashMap<String,Object>();
     CircleImageView imgProfile;
     TextView tvUsername;
     TextInputEditText etName, etLastname, etBirthday, etProfession, etJob, etLocation;
@@ -28,6 +37,16 @@ public class EditProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         connectXml();
+        Bundle bundle = this.getIntent().getExtras();
+        String username = bundle.get("Username").toString();
+        String email= bundle.get("Email").toString();
+        String rol = bundle.get("Rol").toString();
+        if (bundle!=null){
+            tvUsername.setText(username);
+        }
+
+
+
 
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +62,28 @@ public class EditProfileActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(getApplicationContext(), etName.getText().toString() + " " + etLastname.getText().toString(),Toast.LENGTH_LONG).show();
                 }
+
+                String name = etName.getText().toString();
+                String lastname = etLastname.getText().toString();
+                String fenac = etBirthday.getText().toString();
+                String profesion = etProfession.getText().toString();
+                String cargo = etJob.getText().toString();
+                String localidad = etLocation.getText().toString();
+                map.put("Username", username);
+                map.put("Email", email);
+                map.put("Rol",rol);
+                map.put("Name", name);
+                map.put("Lastname", lastname);
+                map.put("FechaNacimiento", fenac);
+                map.put("Profession", profesion);
+                map.put("Job", cargo);
+                map.put("Location", localidad);
+                bd.collection("Users").add(map);
+
+
+
+
+
             }
         });
 
